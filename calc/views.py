1bd010656205166f25400db1612619c02a4f24f8
Abhisek <abhisek.consultant@gmail.com>
2021-05-27 18:49:38 +0530

    views and result.html updated

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def home(request):
 	return render(request,'home.html',{'name':'Abhisek'})
+
+def add(request):
+	pname = request.GET['pname']
+	price = int(request.GET['price'])
+	qty = int(request.GET['qty'])
+	tp = price*qty
+	return render(request,"result.html",{"pname":pname,"price":price,"qty":qty,"tp":tp})
